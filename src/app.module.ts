582f2184c506b0d0a7b7cc5ecd04b504c0dbb273
woomae <91794936+woomae@users.noreply.github.com>
2023-07-03 15:55:42 +0900

    2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+﻿import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
 
 @Module({
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,9 +1,49 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { query } from 'express';
 
 @Controller('movies')
 export class MoviesController {
   @Get()
   getAll() {
     return 'this will return all movies';
+  }
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `we are searching for a movie made after: ${searchingYear}`;
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieid: string) {
+    return `this will return one movie with id: ${movieid}`;
+  }
+
+  @Post()
+  create(@Body() moviedata) {
+    console.log(moviedata);
+    return moviedata;
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieid: string) {
+    return `This wil delete a movie with the id: ${movieid}`;
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieid: string, @Body() updatedata) {
+    return {
+      updatedmovie: movieid,
+      ...updatedata,
+    };
   }
 }
